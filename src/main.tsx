731d1e51d9e6e0a0542663bc51fdb68f5d38ca82
victor-prm <victorpabb@gmail.com>
2025-06-12 12:41:45 +0200

    added font assets and created new theme class for Open Sans

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Home from './views/Home'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
-  </StrictMode>,
+    <Home/>
+  </StrictMode>
 )
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,8 @@
+import '../index.css'
+
 export default function Home() {
 
     return (
-        <h1>Home</h1>
+        <h1 className="bg-hifi-accent text-hifi-gray-dark text-3xl font-open-sans font-bold">Home</h1>
     )
-
 }

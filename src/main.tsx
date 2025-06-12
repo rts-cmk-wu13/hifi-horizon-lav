2347350996788a78e3ddf485409d973ef383f252
victor-prm <victorpabb@gmail.com>
2025-06-12 12:51:11 +0200

    removed unused Home from index.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import './index.css'
-import Home from './views/Home'
 
 import { RouterProvider } from "react-router";
 import router from "./router/router";

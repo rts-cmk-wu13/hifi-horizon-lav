ff00f3f8c743a47c9a91ba266ceb36a9a2a839d3
Loke <U2507983@rts-365.dk>
2025-06-12 10:30:04 +0200

    added config for aliases/absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import path from 'path'
+
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    resolve: {
+        alias: {
+            src: path.resolve(__dirname, 'src'),
+            views: path.resolve(__dirname, 'src/views'),
+        },
+    },
 })

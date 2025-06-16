ec837edb09da1792df2b26e8713e3ff48b51f1b0
Loke <U2507983@rts-365.dk>
2025-06-16 09:19:11 +0200

    home page done for now, info section added

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -22,6 +22,35 @@ export default function Home() {
                     <p>product</p>
                 </div>
             </section>
+
+            <section className="px-12 pt-28 pb-20 flex justify-between bg-hifi-black text-hifi-white text-sm">
+                <div className="max-w-lg *:pt-4">
+                    <h2 className="text-2xl font-bold">What we do</h2>
+                    <p>We look forward to customising a system to meet your needs.</p>
+                    <p>We don’t favour one manufacturer over another – the only thing we do favour is making sure our customers get the right product that suits their needs and listening preferences. We will ask many questions in order to ensure that what you buy from us is tailored to you and you alone.</p>
+                    <p>If you are looking for a product not found in our demonstration showrooms or our online site, don’t fret as we have access to hundreds of brands.</p>
+                    <p>One of our biggest pleasures of working in this industry is to see the smile on our customers’ faces when they finally hear and see the system of their dreams.</p>
+                </div>
+                <div className="max-w-xs mr-14">
+                    <h2 className="text-2xl font-bold mb-9">Opening hours</h2>
+                    <ul>
+                        <li className="font-bold">Edinburgh</li>
+                        <li>2 Joppa Rd,Edinburgh, EH15 2EU</li>
+                        <li>Monday to Friday: 10:00am - 5:30pm</li>
+                        <li>Saturday: 10:00am - 5:30pm</li>
+                        <li>Sunday: Closed</li>
+                    </ul>
+                    <ul className="pt-4">
+                        <li className="font-bold">Falkirk</li>
+                        <li>44 Cow Wynd, Falkirk, Central Region, FK1 1PU</li>
+                        <li>Monday to Friday: 10:00am - 5:30pm</li>
+                        <li>Saturday - By appointment only</li>
+                        <li>Sunday: Closed</li>
+                    </ul>
+                </div>
+            </section>
+
+            <section className="p-9 bg-hifi-gray-lightest"></section>
         </>
     )
 }

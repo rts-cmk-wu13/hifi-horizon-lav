9bb2699d1237f891c2504a3252b49f9afb77fe1d
Loke <U2507983@rts-365.dk>
2025-06-16 10:22:01 +0200

    added checks for icons and moved styling to variable, button done for now

--- src/components/StandardButton.tsx
+++ src/components/StandardButton.tsx
@@ -1,25 +1,38 @@
+import type { ReactNode } from "react"
 import { Link } from "react-router"
-
 
 type StandardButtonProps = {
     obj: {
         text: string,
         href?: string,
         func?: () => void,
+        icon?: ReactNode,
+        width?: string
     }
 }
 
 export default function StandardButton({ obj }: StandardButtonProps) {
+    let buttonStyling = `px-8 py-2 inline-block rounded-sm shadow-[1px_2px_4px] shadow-hifi-black/25 text-hifi-gray-lightest bg-hifi-accent text-sm text-center ${obj.width && obj.width}`
+
+    let buttonText = obj.icon ? (
+        <>
+            {obj.text} {obj.icon}
+        </>
+    ) : (
+        obj.text
+    )
+
+
     return (
         <>
             {obj.href ? (
-                <Link to={obj.href} className="py-2 w-40 inline-block rounded-sm shadow-[1px_2px_4px] shadow-hifi-black/25 text-hifi-gray-lightest bg-hifi-accent text-sm text-center">
-                    {obj.text}
+                <Link to={obj.href} className={buttonStyling}>
+                    {buttonText}
                 </Link>
             ) :
             (
-                <button onClick={obj.func}>
-                    {obj.text}
+                <button onClick={obj.func} className={buttonStyling}>
+                    {buttonText}
                 </button>
             )}
         </>

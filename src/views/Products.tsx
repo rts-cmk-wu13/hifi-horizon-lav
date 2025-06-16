06d07b15bcba100031102d6e28ad6eeaf976df24
Albert Pinnerup <albert.pinnerup@gmail.com>
2025-06-16 13:09:26 +0200

    finished the product card component, and added it to the products page

--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -1,10 +1,19 @@
-
-
+import { useLoaderData } from "react-router";
+import ProductCard from "../components/ProductCard";
+import type { Product } from "../schemas/schemas";
 
 export default function Products() {
 
+    const products = useLoaderData();
 
     return (
-       <h1>Products</h1>
+        <>
+            <h1>Products</h1>
+            <div className="grid grid-cols-[repeat(auto-fit,_minmax(20rem,_1fr))] gap-4 justify-items-center h-full auto-rows-[minmax(20rem,_auto)]">
+                {products.map((product: Product) => (
+                    <ProductCard key={product.id} data={product} config={{showStock:true}}/>
+                ))}
+            </div>
+        </>
     );
 }

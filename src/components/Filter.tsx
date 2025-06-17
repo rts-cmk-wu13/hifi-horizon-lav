8fd8591215b68181b437371db537f3e335d86f40
Albert Pinnerup <albert.pinnerup@gmail.com>
2025-06-17 13:53:28 +0200

    filter functionality for brand and color done, have tried to implement price, but am finding it difficult

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,25 +1,79 @@
-import { useLoaderData } from "react-router"
 import { type ProductList } from "../schemas/schemas";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa6";
 
 type FilterProps = {
     data: ProductList;
-}
+    updateFilters: (type: string, checked: boolean, value: string) => void;
+};
 
-export default function Filter({ data } : FilterProps) {
-
-    const uniqueBrands = Array.from(new Set(data.map(product => product.brand)));
-    const uniqueCategories = Array.from(new Set(data.map(product => product.category)));
-    const uniqueColors = Array.from(new Set(data.map(product => product.color)));
+export default function Filter({ data, updateFilters }: FilterProps) {
+    const filterConfig = {
+        brand: Array.from(new Set(data.map((product) => product.brand))),
+        color: Array.from(new Set(data.map((product) => product.color))),
+    };
 
     return (
-        <div>
-            {uniqueBrands.map((product) => (
-                <label key={product}>
-                    <input type="checkbox" name="brand" value={product}/>
-                    {product}
-                </label>
+        <div className="w-full min-w-[260px]">
+            {Object.entries(filterConfig).map(([filterType, options]) => (
+                <details key={filterType} className="group">
+                    <summary className="capitalize list-none flex items-center justify-between">
+                        {filterType}
+                        <span className="group-open:hidden">
+                            <FaChevronDown />
+                        </span>
+                        <span className="hidden group-open:block">
+                            <FaChevronUp />
+                        </span>
+                    </summary>
+                    {options.map((option, index) => (
+                        <label key={index} className="flex items-center gap-2">
+                            <input
+                                type="checkbox"
+                                onChange={(e) =>
+                                    updateFilters(
+                                        filterType,
+                                        e.target.checked,
+                                        option.toString()
+                                    )
+                                }
+                                className="form-checkbox"
+                            />
+                            {option}
+                        </label>
+                    ))}
+                </details>
             ))}
-        </div>
-    )
+            {/* <details>
+                <summary>Brand</summary>
+                {brands.map((brand, index) => (
+                    <label key={index} className="flex items-center gap-2">
+                        <input
+                            type="checkbox"
+                            onChange={(e) =>
+                                updateFilters("brand", e.target.checked, brand)
+                            }
+                            className="form-checkbox"
+                        />
+                        {brand}
+                    </label>
+                ))}
+            </details>
+            <details>
+                <summary>Color</summary>
 
+                {color.map((color, index) => (
+                    <label key={index} className="flex items-center gap-2">
+                        <input
+                            type="checkbox"
+                            onChange={(e) =>
+                                updateFilters("color", e.target.checked, color)
+                            }
+                            className="form-checkbox"
+                        />
+                        {color}
+                    </label>
+                ))}
+            </details> */}
+        </div>
+    );
 }

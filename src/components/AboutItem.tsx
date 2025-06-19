ebf25dafb1cb1da8b302349a45a2ce3c8ae8bf50
victor-prm <victorpabb@gmail.com>
2025-06-19 11:21:02 +0200

    added basic structure for about page

---
+++ src/components/AboutItem.tsx
@@ -0,0 +1,57 @@
+import type { Components } from 'react-markdown';
+import ReactMarkdown from 'react-markdown';
+
+export type AboutItemData = {
+    id: string;
+    title: string;
+    img: string;
+    subtitle: string;
+    body: string;
+};
+
+export type AboutItemProps = {
+    data: AboutItemData;
+};
+
+export const MarkdownStyle: Components = {
+    p: ({ node, ...props }) => (
+        <p className="text-xs text-gray-700 leading-relaxed" {...props} />
+    ),
+    h1: ({ node, ...props }) => (
+        <h1 className="text-3xl font-bold" {...props} />
+    ),
+    h2: ({ node, ...props }) => (
+        <h2 className="text-2xl font-semibold" {...props} />
+    ),
+    h3: ({ node, ...props }) => (
+        <h3 className="text-xl font-semibold" {...props} />
+    ),
+    h4: ({ node, ...props }) => (
+        <h4 className="text-base font-semibold -mb-3" {...props} />
+    ),
+    strong: ({ node, ...props }) => (
+        <strong className="font-semibold text-gray-900" {...props} />
+    ),
+    ul: ({ node, ...props }) => (
+        <ul className="list-disc pl-5 space-y-1" {...props} />
+    ),
+    li: ({ node, ...props }) => <li className="text-gray-700" {...props} />,
+};
+
+export default function AboutItem({ data }: AboutItemProps) {
+    return (
+        <section id={data.id} className="flex gap-12 *:w-full">
+            <div>
+                <img src={data.img} alt="" className="object-cover h-full"/>
+            </div>
+            <div className="flex flex-col gap-2">
+                <h3 className='text-xl font-semibold'>{data.title}</h3>
+                <p className='text-amber-400 font-semibold'>{data.subtitle}</p>
+                <ReactMarkdown components={MarkdownStyle}>
+                    {data.body}
+                </ReactMarkdown>
+            </div>
+
+        </section>
+    );
+}
--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,11 +1,17 @@
 import PageWrapper from "../components/PageWrapper"
+import AboutItem from "../components/AboutItem";
+import { useLoaderData } from "react-router";
+import { type AboutItemData } from "../components/AboutItem";
 
 export default function About() {
-
+    const data = useLoaderData();
     return (
         <PageWrapper obj={{ heading: "Our History" }}>
-            <div></div>
+            <div className="flex flex-col gap-4 p-8">
+                {data.map((obj: AboutItemData, index: number) => (
+                    <AboutItem key={index} data={obj} />
+                ))}
+            </div>
         </PageWrapper>
     )
-
 }

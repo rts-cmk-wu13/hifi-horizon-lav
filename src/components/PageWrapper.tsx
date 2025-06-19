6445a001dbe41672d28c5b8ee5df9bbc104e27ae
Loke <U2507983@rts-365.dk>
2025-06-19 09:09:46 +0200

    PageWrapper updated

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -6,12 +6,16 @@ type PageWrapperProps = {
         heading: string
     }
     children: ReactNode | null;
+    className: string;
 }
 
 
-export default function PageWrapper({ obj, children }: PageWrapperProps) {
+export default function PageWrapper({ obj, children, className }: PageWrapperProps) {
+
+    const wrapperStyle = `p-hifi-default pb-16 ${className ?? ""}`
+
     return (
-        <div className="p-hifi-default pb-16">
+        <div className={wrapperStyle}>
             <h1 className="mb-12 text-5xl font-semibold uppercase text-hifi-gray-dark">
                 {obj.heading}
             </h1>

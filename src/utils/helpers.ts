0c286bbb31a3520e28f1394af44876ac357f8708
victor-prm <victorpabb@gmail.com>
2025-06-20 13:38:35 +0200

    made checks for local vs live url paths for fetches and img sources

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -14,3 +14,42 @@ export function scrollToHashElement() {
     }
 }
 
+export function getBaseURL(url?: string): string {
+    const locationObj = url ? new URL(url) : window.location;
+    const { protocol, hostname, port } = locationObj;
+    return `${protocol}//${hostname}${port ? `:${port}` : ''}`;
+}
+
+export function isRunningLocal() {
+    //return getBaseURL().includes("localhost") ? true : false;
+    return false;
+}
+
+export function liveOrLocalBaseURL() {
+    let baseURL = ""
+    if (isRunningLocal()) {
+        console.log("local")
+        baseURL = "http://localhost:4000"
+    } else {
+        console.log("live")
+        baseURL = "https://hifi-horizon-api-lav.onrender.com"
+    }
+    console.log(baseURL)
+    return baseURL
+}
+type HasImg = {
+  img: string;
+  [key: string]: any;
+};
+
+export function handleImgPaths<T extends HasImg>(data: T[]): T[] {
+  if (!isRunningLocal()) {
+    data = data.map(item => ({
+      ...item,
+      img: item.img.replace("https://localhost:4000", liveOrLocalBaseURL()),
+    }));
+  }
+  console.log("Handle img paths", data)
+
+  return data;
+}

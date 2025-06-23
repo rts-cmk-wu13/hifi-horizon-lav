d2de00f6b1d8c194de357a61d89141e552ff42af
Loke <U2507983@rts-365.dk>
2025-06-23 13:02:51 +0200

    CtaBanner updated to use children instead of obj.input, WhiteBox used in CtaBanner

--- src/components/CtaBanner.tsx
+++ src/components/CtaBanner.tsx
@@ -1,25 +1,25 @@
 import type { ReactNode } from "react"
 
+import WhiteBox from "./WhiteBox"
+
+
 type CtaBannerProps = {
     obj: {
         heading: string,
         text: string,
-        input: ReactNode,
     }
+    children: ReactNode,
     className?: string
 }
 
 
-export default function CtaBanner({ obj, className }: CtaBannerProps) {
-
-    const bannerStyle = `py-8 flex flex-col items-center shadow-[2px_4px_4px] shadow-hifi-black/25 bg-hifi-white ${className ?? ""}`
-
-
+export default function CtaBanner({ obj, children, className }: CtaBannerProps) {
     return (
-        <div className={bannerStyle}>
+        <WhiteBox className={`flex flex-col items-center ${className ?? ""}`}>
             <h2 className="text-2xl font-semibold">{obj.heading}</h2>
             <p className="mb-6 text-sm">{obj.text}</p>
-            {obj.input}
-        </div>
+
+            {children}
+        </WhiteBox>
     )
 }

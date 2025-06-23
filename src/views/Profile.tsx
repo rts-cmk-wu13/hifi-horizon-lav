b8593d5e8102d2add34ba6f9ac7515d904202dcc
Loke <U2507983@rts-365.dk>
2025-06-23 12:41:29 +0200

    profile page layout done

--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Form } from "react-router";
 import { FaUser, FaPhoneAlt, FaEnvelope, FaInfoCircle } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 
@@ -59,8 +60,7 @@ const userInfoSections = [
         "id": "marketing",
         "title": "Marketing",
         "icon": <FaInfoCircle />,
-        "body": "Accept marketing from HiFi Horizon (newsletter and discount offers by email).",
-        "hidden": true
+        "body": "Marketing from HiFi Horizon (newsletter and discount offers by email)."
     }
 ]
 
@@ -85,38 +85,38 @@ export default function Profile() {
                     </div>
                 </div>
 
-                <form action="" id="profileForm" className="flex flex-col">
+                <Form method="PATCH" noValidate id="profileForm" className="flex flex-col">
                     <ul>
                         {userInfoSections.map((info, i) => (
-                                <li key={i} className="py-12 flex justify-between border-hifi-gray-light border-b-2 last:border-b-0">
-                                    <div className="flex items-center gap-8 w-full">
-                                        <span className="text-4xl mt-1">
-                                            {info.icon}
-                                        </span>
+                            <li key={i} className="py-8 flex justify-between border-hifi-gray-light border-b-2 last:border-b-0">
+                                <div className="flex items-center gap-8 w-full">
+                                    <span className="text-4xl">
+                                        {info.icon}
+                                    </span>
 
-                                        <div className="w-full *:last:h-9">
-                                            <h3 className="font-bold">{info.title}</h3>
+                                    <div className="w-full *:last:h-9">
+                                        <h3 className="font-bold">{info.title}</h3>
 
-                                            {(editFields && info.id !== "marketing") ? (
-                                                <input type="text" name={info.id} id={info.id} defaultValue={info.body ? info.body : String(userDummy[info.id as keyof typeof userDummy])} className="px-3 w-full rounded-sm bg-hifi-gray-light shadow-hifi-sm focus:outline-0" />
-                                            ) : (editFields && info.id === "marketing") ? (
-                                                <select name="marketing" id="marketing" className="px-3 w-full rounded-sm bg-hifi-gray-light shadow-hifi-sm">
-                                                    <option value="true">Accept marketing from HiFi Horizon (newsletter and discount offers by email).</option>
-                                                    <option value="false">Don't accept marketing from HiFi Horizon (newsletter and discount offers by email).</option>
-                                                </select>
-                                            ) : (
-                                                <p>
-                                                    {info.body ? info.body : userDummy[info.id as keyof typeof userDummy]}
-                                                </p>
-                                            )}
-                                        </div>
+                                        {(editFields && info.id !== "marketing") ? (
+                                            <input type="text" name={info.id} id={info.id} defaultValue={info.body ? info.body : String(userDummy[info.id as keyof typeof userDummy])} className="px-3 w-full rounded-sm bg-hifi-gray-light shadow-hifi-sm focus:outline-0" />
+                                        ) : (editFields && info.id === "marketing") ? (
+                                            <select name="marketing" id="marketing" className="px-3 w-full rounded-sm bg-hifi-gray-light shadow-hifi-sm">
+                                                <option value="true">Accept marketing from HiFi Horizon (newsletter and discount offers by email).</option>
+                                                <option value="false">Don't accept marketing from HiFi Horizon (newsletter and discount offers by email).</option>
+                                            </select>
+                                        ) : (
+                                            <p className="content-center">
+                                                {info.body ? info.body : userDummy[info.id as keyof typeof userDummy]}
+                                            </p>
+                                        )}
                                     </div>
-                                </li>
-                            ))}
+                                </div>
+                            </li>
+                        ))}
                     </ul>
 
-                    {editFields && <StandardButton obj={{ text: "Save changes", func: handleEditFields}} className="self-end" />}
-                </form>
+                    {editFields && <StandardButton obj={{ text: "Save changes", func: handleEditFields }} className="self-end" />}
+                </Form>
             </WhiteBox>
         </div>
     )
